4231e2b85935acd45709d26ced4a125870362796
Abhai Kollara <abhai.dilip@gmail.com>
2020-10-04 17:23:34 +0530

    Add poll struct

--- types.go
+++ types.go
@@ -446,7 +446,21 @@ type PollAnswer struct {
 
 // Poll contains information about a poll.
 // TODO
-type Poll struct{}
+type Poll struct {
+	ID                    string           `json:"id"`
+	Question              string           `json:"question"`
+	Options               *[]PollOption    `json:"options"`
+	TotalVoterCount       int              `json:"total_voter_count"`
+	IsClosed              bool             `json:"is_closed"`
+	IsAnonymous           bool             `json:"is_anonymous"`
+	Type                  string           `json:"json"`
+	AllowsMultipleAnswers bool             `json:"allows_multiple_answers"`
+	CorrectOptionID       int              `json:"correct_option_id"`
+	Explanation           string           `json:"explanation"`
+	ExplanationEntities   *[]MessageEntity `json:"explanation_entities"`
+	OpenPeriod            int              `json:"open_period"`
+	CloseDate             int              `json:"close_date"`
+}
 
 // Location contains information about a place.
 type Location struct {
